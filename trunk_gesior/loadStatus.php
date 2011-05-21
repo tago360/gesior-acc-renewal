703be8344ca08d8272af756151627c10b4c31bee
phoowned@wp.pl <phoowned@wp.pl@54f82feb-0c4b-026f-beb6-7fbe076e180a>
2011-05-21 16:17:15 +0000

    server status use new cache folder and class ConfigPHP to handel parameters

---
+++ trunk_gesior/loadStatus.php
@@ -0,0 +1,33 @@
+<?PHP
+$statusTimeout = max(1, $configServer->getValue('statusTimeout') / 1000);
+$serverStatusCache = new ConfigPHP('./cache/serverstatus');
+if($serverStatusCache->getValue('lastCheck'] + $statusTimeout < time())
+{
+	$serverStatusCache->setValue('lastCheck', $statusTimeout);
+	$sock = @fsockopen($configServer->getValue('ip'), $configServer->getValue('port'), $errno, $errstr, 1);
+	if($sock)
+	{
+		fwrite($sock, chr(6).chr(0).chr(255).chr(255).'info');
+		$data = '';
+		while(!feof($sock))
+			$data .= fgets($sock, 1024);
+		fclose($sock);
+		$serverStatusCache->setValue('online', true);
+		preg_match('/players online="(\d+)" max="(\d+)"/', $data, $matches);
+		$serverStatusCache->setValue('players', $matches[1]);
+		$serverStatusCache->setValue('playersMax', $matches[2]);
+		preg_match('/uptime="(\d+)"/', $data, $matches);
+		$serverStatusCache->setValue('serverStatus_uptime'], floor($matches[1] / 3600) . 'h ' . floor(($matches[1] - $h*3600) / 60). 'm';
+		preg_match('/monsters total="(\d+)"/', $data, $matches);
+		$serverStatusCache->setValue('monsters', $matches[1]);
+	}
+	else
+	{
+		$serverStatusCache->setValue('online', false);
+		$serverStatusCache->setValue('players', 0);
+		$serverStatusCache->setValue('playersMax', 0);
+	}
+	$serverStatusCache->saveToFile();
+}
+?>
+
